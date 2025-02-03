86194f2a5a58ef2190262a64bf292d6f92832830
sofiiaru <45317055+sofiiaru@users.noreply.github.com>
2025-02-03 16:42:46 -0500

    Time: 0 ms (100%), Space: 44.3 MB (87.57%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,16 +12,25 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null) return false;
-        //brute force solution
+        //BRUTE FORCE solution
+        // List<ListNode> visited = new ArrayList<>(); 
 
-        List<ListNode> visited = new ArrayList<>(); 
+        // ListNode cur = head;
+        // while(cur.next != null){
+        //     if(visited.contains(cur)) return true;
+        //     else visited.add(cur);
+        //     cur = cur.next;
+        // }
+        // return false;
 
-        ListNode cur = head;
-        while(cur.next != null){
-            if(visited.contains(cur)) return true;
-            else visited.add(cur);
-            cur = cur.next;
-        }
-        return false;
+        //TWO POINTERS solution
+        ListNode slow, fast;
+        slow = fast = head;
+        while(fast != null && fast.next != null){
+             slow = slow.next;
+             fast = fast.next.next;
+             if(slow == fast) return true;
+         }
+         return false;
     }
 }
