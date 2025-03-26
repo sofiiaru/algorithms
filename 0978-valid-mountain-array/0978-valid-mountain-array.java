74f1fbae7547b2d9b5ec872a0782542303682cbc
sofiiaru <45317055+sofiiaru@users.noreply.github.com>
2025-03-26 17:23:30 -0400

    Time: 3 ms (19.23%), Space: 45.2 MB (87.41%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.java
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if(arr.length < 3) return false;
+        int length = arr.length;
+        if(length < 3) return false;
         boolean peakFound = false;
         boolean increasingFound = false;
-        for(int i = 1; i< arr.length; i++) {
+        for(int i = 1; i< length; i++) {
             if(arr[i] == arr[i-1]) return false;
             else if(arr[i] > arr[i-1] && !peakFound) increasingFound = true;
             else if(arr[i] > arr[i-1] && peakFound) return false;
